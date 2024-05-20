c817451ba383d1fb2c6362c391d233de2ff3cb2c
Vignesh-kumar-k <vigneshvicky84321@gmail.com>
2024-05-20 21:35:35 +0530

    for vercel server

--- server/index.js
+++ server/index.js
@@ -9,53 +9,47 @@ const app = express()
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
 
-app.use(express.json()) // transfered data (from backend to frontend ) => json format
+app.use(express.json())
 app.use(cors())
-
 
 mongoose.connect("mongodb+srv://tamilarasand:uzMo3Zc7s0nlfMYC@authority.bai3y9l.mongodb.net/?retryWrites=true&w=majority&appName=authority")
 
-app.post("/login",(req,res)=>{
-    const {email,password} = req.body;
-    authoritymodel.findOne({email:email})
-    .then((user=>{
-        if(user){
-            if(user.password === password) //if the found email's password and the response password is same..
-            {
-                res.json('Success')
-            }else{
-                res.json('The email or password is incorrect')
+app.post("/api/login", (req, res) => {
+    const { email, password } = req.body;
+    authoritymodel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                if (user.password === password) {
+                    res.json('Success')
+                } else {
+                    res.json('The email or password is incorrect')
+                }
+            } else {
+                res.json('no record found')
             }
-        }
-        else{
-            res.json('no record found')
-        }
-    }))
-})
-app.post('/register',(req,res)=>{
-    authoritymodel.create(req.body)
-    .then(authorities=> res.json(authorities))
-    .catch(err =>res.json(err))
-
-})
-
-app.post('/submitcomplaint', (req, res) => {
-    const { name, phonenumber, address,distName, review, images } = req.body;
-
-    try {
-        
-        complaintmodel.create(req.body)
-        .then(complaints => {
-            res.json(complaints );
         })
-        .catch(err =>res.json(err))
+})
+app.post('/api/register', (req, res) => {
+    authoritymodel.create(req.body)
+        .then(authorities => res.json(authorities))
+        .catch(err => res.json(err))
+})
+
+app.post('/api/submitcomplaint', (req, res) => {
+    const { name, phonenumber, address, distName, review, images } = req.body;
+    try {
+        complaintmodel.create(req.body)
+            .then(complaints => {
+                res.json(complaints);
+            })
+            .catch(err => res.json(err))
     } catch (error) {
         console.error(error);
         res.status(500).json('Internal server error');
     }
 });
 
-app.get('/complaints', (req, res) => {
+app.get('/api/complaints', (req, res) => {
     complaintmodel.find({})
         .then(complaints => {
             res.json(complaints);
@@ -66,9 +60,7 @@ app.get('/complaints', (req, res) => {
         });
 });
 
-
-
-const PORT = process.env.PORT || 3001; 
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
